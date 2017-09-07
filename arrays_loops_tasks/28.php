4a0f024f6aa2704b674c2ce3369536e591803a08
dmitry.dev <dmitry.dev@gmail.com>
2017-09-07 23:27:01 +0300

    01_arrays 28 - html table added

--- arrays_loops_tasks/28.php
+++ arrays_loops_tasks/28.php
@@ -28,3 +28,22 @@ for ($i = 0; $i < $limit; $i++) {
     }
     echo PHP_EOL;
 }
+echo "</pre>";
+
+$table = "<table border='1'>";
+$limit = 11;
+for ($i = 0; $i < $limit; $i++) {
+    $table .= '<tr>';
+    for ($j = 0; $j < $limit; $j++) {
+        if ($i == 0) {
+            $composition = $j;
+        } elseif ($j == 0) {
+            $composition = $i;
+        } else {
+            $composition = $i * $j;
+        }
+        $table .= "<td>".$composition."</td>";
+    }
+    $table .= '<tr>';
+}
+echo $table;
