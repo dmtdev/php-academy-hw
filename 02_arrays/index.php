aa13fbc4801b9e8722f8563edb57e53dbe380272
dmitry.dev <dmitry.dev@gmail.com>
2017-09-07 23:15:13 +0300

    02_arrays 9-14 done

--- 02_arrays/index.php
+++ 02_arrays/index.php
@@ -16,7 +16,7 @@ foreach ($array as $value) {
         $wordsCount++;
     }
 }
-//var_dump($wordsCount);
+var_dump($wordsCount);
 
 //Task 2
 
@@ -29,7 +29,7 @@ for ($i = 0; $i < strlen($string); $i++) {
         $result[$i] = $string[$i];
     }
 }
-//var_dump($result);
+var_dump($result);
 
 //Task 3
 $string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
