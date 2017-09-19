492635fba4ae15d6ac39645171da53ccc8113100
dmitry.dev <dmitry.dev@gmail.com>
2017-09-19 14:40:24 +0300

    functions part 2 2-5, 7 ready, 1 and 6 - :((

--- 01_arrays/11.php
+++ 01_arrays/11.php
@@ -5,11 +5,6 @@
  * Date: 05.09.2017
  * Time: 20:10
  */
-function recursiveToNum($n) {
-    if ($n != 0) {
-        recursiveToNum(--$n);
-        echo $n + 1 . " ";
-    }
-}
-
-recursiveToNum(100);
+$string = "123";
+unset($string[0]);
+echo $string;
--- 02_functions_hw2/index.php
+++ 02_functions_hw2/index.php
@@ -10,29 +10,32 @@
 //Task 1
 $array = array('one', 'two', 'three', 'four', 'five', 'six');
 //($previousElem, $nextElem) use ($array)
-$counter = 0;
+//$counter = 0;
 //$previousElem, $nextElem
-usort($array, function ($previousElem, $nextElem) use (&$counter) {
-    if ($counter % 2 == 0) {
-        $counter += 2;
-        return 1;
-    }
-//    else {
-//        $counter++;
+usort($array, function ($previousElem, $nextElem) use ($array) {
+    var_dump(array_search($previousElem,$array));
+    var_dump(array_search($nextElem,$array));
+//    static $counter = 0;
+//    //static $subCounter = 1;
+//    $counter = array_search($previousElem, $array) ;
+//    //$counter -= $subCounter;
+//    $counter++;
+//    var_dump($counter);
+//    if ($counter % 2 == 0) {
+//        return 1;
+//    } else {
 //        return -1;
 //    }
-//    return 0;
 });
 
-//var_dump($array);
+var_dump($array);
 
-//die();
 
 //Task 2 - часто возвращает пустой массив, rand - для ускорения создания массива
 $array = range(1, 1000000, rand(1, 10000));
 
-$array = array_filter($array, function ($elem) use ($array) {
-    if ($elem % 2 == 0 and !$elem % 3 == 0 and $elem % 5 == 0) {
+$array = array_filter($array, function ($elem) {
+    if ($elem % 2 == 0 and $elem % 3 == 0 and $elem % 5 == 0) {
         return 1;
     }
     return 0;
@@ -63,7 +66,6 @@ function sayHello(string $name)
     echo "Всего поздоровались " . $count . " раз" . PHP_EOL;
 }
 
-//
 sayHello("Dmitry");
 sayHello("Ivan");
 sayHello("Jon");
@@ -87,40 +89,42 @@ echo average(3) . PHP_EOL;
 echo average(31) . PHP_EOL;
 
 //Task 6
-function isPalindrome($string)
-{
-    static $charCount = 0;
-    static $palindrome = "";
-    $charCount = strlen($string) - 1;
-    var_dump($charCount);
-    if ($charCount > 0) {
-        $palindrome .= $string[$charCount];
-        $charCount--;
-        isPalindrome($string);
-    } else {
-        if ($string == $palindrome) {
-            echo "Да" . PHP_EOL;
-        } else {
-            echo "Нет" . PHP_EOL;
-        }
-    }
-}
+//function isPalindrome($string)
+//{
+//    static $charCount = 0;
+//    static $palindrome = "";
+//    static $charIndex;
+//    $charCount = strlen($string);
+//    $charIndex = strlen($string) - 1;
+//    if ($charCount > 0) {
+//        $palindrome .= $string[$charIndex];
+//        $charIndex--;
+//        return isPalindrome($string);
+//    } else {
+//        if ($string == $palindrome) {
+//            return "Да" . PHP_EOL;
+//        } else {
+//            return "Нет" . PHP_EOL;
+//        }
+//    }
+//}
 
-//isPalindrome("abba");
-//isPalindrome("palindrome");
+//echo isPalindrome("abba");
+//echo isPalindrome("palindrome");
 
-//Task
-function numRevers(&$number)
+//Task 7
+function numRevers($number)
 {
     static $result = 0;
-    if ($number > 0) {
-        $result = ($result + ($number % 10)) * 10;
-        $number = ($number - ($number % 10)) / 10;
-        return numRevers($number);
+    static $num = 0;
+    $num = $number;
+    if ($num > 0) {
+        $result = ($result + ($num % 10)) * 10;
+        $num = ($number - ($num % 10)) / 10;
+        return numRevers($num);
     } else {
         return $result / 10;
     }
 }
 
-$num = 321;
-echo numRevers($num);
+echo numRevers(2050801);
